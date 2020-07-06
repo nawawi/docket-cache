21e37ed436f1b5f72a54ffdbd8e65f139ee4a045
Nawawi Jamili <nawawi@rutweb.com>
2020-07-07 01:20:32 +0800

    updated

--- includes/admin/page.php
+++ includes/admin/page.php
@@ -46,40 +46,36 @@ if ($is_debug) {
             <h2 class="title"><?php _e('Overview', 'docket-cache'); ?></h2>
 
             <table class="form-table">
-                <tbody>
-                    <tr>
-                        <th><?php _e('Status', 'docket-cache'); ?></th>
-                        <td><code><?php echo $status_text; ?></code></td>
-                    </tr>
+                <tr>
+                    <th><?php _e('Status', 'docket-cache'); ?></th>
+                    <td><code><?php echo $status_text; ?></code></td>
+                </tr>
 
-                    <tr>
-                        <th><?php _e('OPCache', 'docket-cache'); ?></th>
-                        <td><code><?php echo $this->status_code[$this->get_opcache_status()]; ?></code></td>
-                    </tr>
+                <tr>
+                    <th><?php _e('OPCache', 'docket-cache'); ?></th>
+                    <td><code><?php echo $this->status_code[$this->get_opcache_status()]; ?></code></td>
+                </tr>
 
-                    <tr>
-                        <th><?php _e('Memory', 'docket-cache'); ?></th>
-                        <td><code><?php echo $this->get_mem_size(); ?></code></td>
-                    </tr>
+                <tr>
+                    <th><?php _e('Memory', 'docket-cache'); ?></th>
+                    <td><code><?php echo $this->get_mem_size(); ?></code></td>
+                </tr>
 
-                    <?php if (1 === $status): ?>
-                    <tr>
-                        <th><?php _e('Cache Size', 'docket-cache'); ?></th>
-                        <td><code><?php echo $this->get_dirsize(); ?></code></td>
-                    </tr>
-                    <?php endif; ?>
-                </tbody>
+                <?php if (1 === $status): ?>
+                <tr>
+                    <th><?php _e('Cache Size', 'docket-cache'); ?></th>
+                    <td><code><?php echo $this->get_dirsize(); ?></code></td>
+                </tr>
+                <?php endif; ?>
             </table>
 
             <p class="submit">
-
-                <?php if (!$this->has_dropin()) : ?>
-                    <a href="<?php echo $this->action_query('enable-cache'); ?>" class="button button-primary button-large"><?php _e('Enable Object Cache', 'docket-cache'); ?></a>
-                <?php elseif ($this->validate_dropin()) : ?>
-                    <a href="<?php echo $this->action_query('flush-cache'); ?>" class="button button-primary button-large"><?php _e('Flush Cache', 'docket-cache'); ?></a>&nbsp;&nbsp;
-                    <a href="<?php echo $this->action_query('disable-cache'); ?>" class="button button-secondary button-large"><?php _e('Disable Object Cache', 'docket-cache'); ?></a>
+            <?php if (!$this->has_dropin()) : ?>
+                <a href="<?php echo $this->action_query('enable-cache'); ?>" class="button button-primary button-large"><?php _e('Enable Object Cache', 'docket-cache'); ?></a>
+            <?php elseif ($this->validate_dropin()) : ?>
+                <a href="<?php echo $this->action_query('flush-cache'); ?>" class="button button-primary button-large"><?php _e('Flush Cache', 'docket-cache'); ?></a>&nbsp;&nbsp;
+                <a href="<?php echo $this->action_query('disable-cache'); ?>" class="button button-secondary button-large"><?php _e('Disable Object Cache', 'docket-cache'); ?></a>
             <?php endif; ?>
-
             </p>
         </div>
 
@@ -89,16 +85,11 @@ if ($is_debug) {
         <div class="section option">
             <h2 class="title"><?php _e('Options', 'docket-cache'); ?></h2>
 
-    <p>
-<h4><?php _e('Configuration Options', 'docket-cache'); ?></h4>
+            <h4><?php _e('Configuration Options', 'docket-cache'); ?></h4>
+            <?php _e('To adjust the configuration, please see the <a href="https://github.com/nawawi/docket-cache#configuration-options" rel="noopener" target="_blank">configuration options</a> for a full list.', 'docket-cache'); ?>
 
-<?php _e('To adjust the configuration, please see the <a href="https://github.com/nawawi/docket-cache#configuration-options" rel="noopener" target="_blank">configuration options</a> for a full list.', 'docket-cache'); ?>
-
-<h4><?php _e('WP-CLI Commands', 'docket-cache'); ?> </h4>
-
-<?php _e('To use the WP-CLI commands, please see the <a href="https://github.com/nawawi/docket-cache#wp-cli-commands" rel="noopener" target="_blank">WP-CLI commands</a> for a full list.', 'docket-cache'); ?>
-    </p>
-
+            <h4><?php _e('WP-CLI Commands', 'docket-cache'); ?> </h4>
+            <?php _e('To use the WP-CLI commands, please see the <a href="https://github.com/nawawi/docket-cache#wp-cli-commands" rel="noopener" target="_blank">WP-CLI commands</a> for a full list.', 'docket-cache'); ?>
         </div>
     <?php endif; ?>
 
@@ -108,35 +99,31 @@ if ($is_debug) {
             <h2 class="title"><?php _e('Overview', 'docket-cache'); ?></h2>
 
             <table class="form-table">
-                <tbody>
-                    <tr>
-                        <th><?php _e('Status', 'docket-cache'); ?></th>
-                        <td><code><?php echo $this->status_code[$is_debug ? 1 : 0]; ?></code></td>
-                    </tr>
-                    <tr>
-                        <th><?php _e('Log File', 'docket-cache'); ?></th>
-                        <td><code><?php echo DOCKET_CACHE_DEBUG_FILE; ?></code></td>
-                    </tr>
-                                        <tr>
+                <tr>
+                    <th><?php _e('Status', 'docket-cache'); ?></th>
+                    <td><code><?php echo $this->status_code[$is_debug ? 1 : 0]; ?></code></td>
+                </tr>
+                <?php if (!empty($output)): ?>
+                <tr>
+                    <th><?php _e('Log File', 'docket-cache'); ?></th>
+                    <td><code><?php echo DOCKET_CACHE_DEBUG_FILE; ?></code></td>
+                </tr>
+                <?php endif; ?>
+                <tr>
                     <td colspan="2" class="output">
-
-    <?php if ($is_debug) {
-    if (empty($output)) {
-        echo '<p><code>'.__('Not available', 'docket-cache').'</code></p>';
-    } else {
-        echo '<textarea id="log" class="code" readonly="readonly" rows="20">'.implode("\n", array_reverse($output, true)).'</textarea>';
-    }
-}
-    ?>
-</td></tr>
-                </tbody>
+                    <?php
+                        if (empty($output)) {
+                            echo '<p><code>'.__('Not available', 'docket-cache').'</code></p>';
+                        } else {
+                            echo '<textarea id="log" class="code" readonly="readonly" rows="20">'.implode("\n", array_reverse($output, true)).'</textarea>';
+                        }
+                    ?>
+                    </td>
+                </tr>
             </table>
 
             <p class="submit">
-                <a href="#" onclick="window.location.assign(window.location.href); return false;" class="button button-primary button-large"><?php _e('Refresh', 'docket-cache'); ?></a>&nbsp;
-                <?php /*if (!empty($output)): ?>
-                    <a href="<?php echo get_home_url(null, '/wp-content/object-cache.log').'?'.time(); ?>" target="_blank" rel="noopener" class="button button-primary button-large"><?php _e('Download','docket-cache');?></a>
-                <?php endif;*/ ?>
+                <a href="<?php echo network_admin_url(add_query_arg('tab', $tab, $this->page)); ?>" class="button button-primary button-large"><?php _e('Refresh', 'docket-cache'); ?></a>&nbsp;
             </p>
         </div>
 
