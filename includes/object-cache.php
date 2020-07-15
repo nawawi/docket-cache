e2e16f365a66018962b7f5816aaf3b7cdaaba7e8
Nawawi Jamili <nawawi@rutweb.com>
2020-07-16 05:44:01 +0800

    bump 20.07.16

--- includes/object-cache.php
+++ includes/object-cache.php
@@ -3,7 +3,7 @@
  * @wordpress-plugin
  * Plugin Name:         Docket Cache Drop-in
  * Plugin URI:          http://wordpress.org/plugins/docket-cache/
- * Version:             20.07.14
+ * Version:             20.07.16
  * Description:         A file-based persistent WordPress Object Cache stored as a plain PHP code.
  * Author:              Nawawi Jamili
  * Author URI:          https://rutweb.com
